42c38b99a4cb13047c3c72b91f9ca68f34fef8a7
person75492 <person75492@gmail.com>
2026-04-21 16:41:32 +0530

    fix cors for vercel

--- task-manager/server.js
+++ task-manager/server.js
@@ -20,7 +20,14 @@ const app = express();
 app.use(helmet({ contentSecurityPolicy: false }));
 
 // Body parsing
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'], credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    /\.vercel\.app$/,   // allow all vercel deployments
+  ],
+  credentials: true,
+}));
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: false }));
 
